a52dcb0d09f935de09b2907c127166be141da307
Jeff Kaminski <jpkamins@us.ibm.com>
2020-02-06 12:26:59 -0500

    Updates to bash and python LM customization scripts.

--- speech-to-text/testSTTcustom.py
+++ speech-to-text/testSTTcustom.py
@@ -7,26 +7,32 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-##########################################################################
-# Add your IBM Cloud credentials here
-# o If you use IAM service credentials, leave USERNAME set to "apikey"
-#   and set PASSWORD to the value of your IAM API key.
-# o If you use pre-IAM service credentials, set the values to your USERNAME
-#   and PASSWORD.
-# See the following instructions for getting your own credentials:
-#   https://console.bluemix.net/docs/services/watson/getting-started-credentials.html
-##########################################################################
-username = "apikey"
-password = "iam_apikey"
 headers = {'Content-Type' : "application/json"}
 
 ##########################################################################
-# Step 1: Create a custom model
-# Change "name" and "description" to suit your own model
+# Add your IBM Cloud service credentials here.
+# o If you use IAM service credentials, leave 'username' set to "apikey"
+#   and set 'password' to the value of your IAM API key.
+# o If you use pre-IAM service credentials, set the values to your
+#   'username' and 'password'.
+# Also set 'url' to the URL for your service instance as provided in your
+# service credentials.
+# See the following instructions for getting your own credentials:
+#   https://cloud.ibm.com/docs/watson?topic=watson-iam
 ##########################################################################
+
+username = "apikey"
+password = "YOUR_IAM_APIKEY"
+url = "YOUR_URL"
+
+##########################################################################
+# Step 1: Create a custom model
+# Change "name" and "description" to suit your own model.
+##########################################################################
+
 print "\nCreating custom mmodel..."
 data = {"name" : "Custom model #1", "base_model_name" : "en-US_BroadbandModel", "description" : "My first STT custom model"}
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations"
+uri = url+"/v1/customizations"
 jsonObject = json.dumps(data).encode('utf-8')
 resp = requests.post(uri, auth=(username,password), verify=False, headers=headers, data=jsonObject)
 
@@ -41,19 +47,21 @@ customID = respJson['customization_id']
 print "Model customization_id: ", customID
 
 ##########################################################################
-# Step 2: Add a corpus file (plain text file - ideally one sentence per line,
-# but not necessary). In this example, we name it 'corpus1' - you can name
-# it whatever you want (no spaces) - if adding more than one corpus, add
-# them with different names
+# Step 2: Add a corpus file (plain text file)
+# Ideally, add one sentence per line, but this is not necessary. In this
+# example, we name it 'corpus1' - you can name it whatever you want (no
+# spaces) - if adding more than one corpus, add them with different names.
 ##########################################################################
+
 # 'corpus.txt' is name of local file containing the corpus to be uploaded
 # 'corpus1' is the name of the new corpus
 # >>>> REPLACE THE VALUES BELOW WITH YOUR OWN CORPUS FILE AND NAME
+
 corpus_file = "corpus.txt"
 corpus_name = "corpus1"
 
 print "\nAdding corpus file..."
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations/"+customID+"/corpora/"+corpus_name
+uri = url+"/v1/customizations/"+customID+"/corpora/"+corpus_name
 with open(corpus_file, 'rb') as f:
    r = requests.post(uri, auth=(username,password), verify=False, headers=headers, data=f)
 
@@ -64,13 +72,14 @@ if r.status_code != 201:
    sys.exit(-1)
 
 ##########################################################################
-# Step 3: Get status of corpus file just added.
+# Step 3: Get status of corpus file just added
 # After corpus is uploaded, there is some analysis done to extract OOVs.
 # One cannot upload a new corpus or words while this analysis is on-going so
 # we need to loop until the status becomes 'analyzed' for this corpus.
 ##########################################################################
+
 print "Checking status of corpus analysis..."
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations/"+customID+"/corpora/"+corpus_name
+uri = url+"/v1/customizations/"+customID+"/corpora/"+corpus_name
 r = requests.get(uri, auth=(username,password), verify=False, headers=headers)
 respJson = r.json()
 status = respJson['status']
@@ -88,10 +97,12 @@ print "Corpus analysis done!"
 ##########################################################################
 # Show all OOVs found
 # This step is only necessary if user wants to look at the OOVs and
-# validate the auto-added sounds-like field. Probably a good thing to do though.
+# validate the auto-added sounds-like field. Probably a good thing to do
+# though.
 ##########################################################################
+
 print "\nListing words..."
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations/"+customID+"/words?sort=count"
+uri = url+"/v1/customizations/"+customID+"/words?sort=count"
 r = requests.get(uri, auth=(username,password), verify=False, headers=headers)
 print "Listing words returns: ", r.status_code
 file=codecs.open(customID+".OOVs.corpus", 'wb', 'utf-8')
@@ -101,13 +112,14 @@ print "Words list from added corpus saved in file: "+customID+".OOVs.corpus"
 ##########################################################################
 # Step 4: Add a single user word
 # One can pass sounds_like and display_as fields or leave empty (if empty
-# the service will try to create its own version of sounds_like)
+# the service will try to create its own version of sounds_like).
 ##########################################################################
+
 print "\nAdding single word..."
 data = {"sounds_like" : ["T. C. P. I. P."], "display_as" : "TCP/IP"}
 wordToAdd = "tcpip"
 u = unicode(wordToAdd, "utf-8")
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations/"+customID+"/words/"+u
+uri = url+"/v1/customizations/"+customID+"/words/"+u
 jsonObject = json.dumps(data).encode('utf-8')
 r = requests.put(uri, auth=(username,password), verify=False, headers=headers, data=jsonObject)
 
@@ -117,16 +129,17 @@ print "Single word added!"
 # Alternatively, one can add multiple words in one request
 print "\nAdding multiple words..."
 data = {"words" : [{"word" : "IEEE", "sounds_like" : ["I. triple E."], "display_as" : "IEEE"}, {"word" : "hhonors", "sounds_like" : ["H. honors", "hilton honors"], "display_as" : "HHonors"}]}
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations/"+customID+"/words"
+uri = url+"/v1/customizations/"+customID+"/words"
 jsonObject = json.dumps(data).encode('utf-8')
 r = requests.post(uri, auth=(username,password), verify=False, headers=headers, data=jsonObject)
 
 print "Adding multiple words returns: ", r.status_code
 
 ##########################################################################
-# Get status of model - only continue to training if 'ready'
+# Get status of model - only continue to training if 'ready'.
 ##########################################################################
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations/"+customID
+
+uri = url+"/v1/customizations/"+customID
 r = requests.get(uri, auth=(username,password), verify=False, headers=headers)
 respJson = r.json()
 status = respJson['status']
@@ -144,7 +157,7 @@ print "Multiple words added!"
 
 # Show all words added so far
 print "\nListing words..."
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations/"+customID+"/words?word_type=user&sort=alphabetical"
+uri = url+"/v1/customizations/"+customID+"/words?word_type=user&sort=alphabetical"
 r = requests.get(uri, auth=(username,password), verify=False, headers=headers)
 
 print "Listing user-added words returns: ", r.status_code
@@ -157,8 +170,9 @@ print "Words list from user-added words saved in file: "+customID+".OOVs.user"
 # After starting this step, need to check its status and wait until the
 # status becomes 'available'.
 ##########################################################################
+
 print "\nTraining custom model..."
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations/"+customID+"/train"
+uri = url+"/v1/customizations/"+customID+"/train"
 data = {}
 jsonObject = json.dumps(data).encode('utf-8')
 r = requests.post(uri, auth=(username,password), verify=False, data=jsonObject)
@@ -169,9 +183,10 @@ if r.status_code != 200:
    sys.exit(-1)
 
 ##########################################################################
-# Get status of training and loop until done
+# Get status of training and loop until done.
 ##########################################################################
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations/"+customID
+
+uri = url+"/v1/customizations/"+customID
 r = requests.get(uri, auth=(username,password), verify=False, headers=headers)
 respJson = r.json()
 status = respJson['status']
@@ -187,7 +202,7 @@ while (status != 'available'):
 print "Training complete!"
 
 print "\nGetting custom models..."
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations"
+uri = url+"/v1/customizations"
 r = requests.get(uri, auth=(username,password), verify=False, headers=headers)
 
 print "Get models returns: ", r.status_code
@@ -196,18 +211,19 @@ print r.text
 sys.exit(0)
 
 ##########################################################################
-# STEP 6 (OPTIONAL): TO LIST AND DELETE THE CUSTOM MODEL:
+# Step 6 (OPTIONAL): TO LIST AND DELETE THE CUSTOM MODEL:
 # Comment the previous call to 'sys.exit(0)'; useful for experimentation
-# with multiple test models
+# with multiple test models.
 ##########################################################################
+
 print "\nDeleting custom model..."
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations/"+customID
+uri = url+"/v1/customizations/"+customID
 r = requests.delete(uri, auth=(username,password), verify=False, headers=headers)
 respJson = r.json()
 print "Model deletion returns: ", resp.status_code
 
 print "\nGetting custom models..."
-uri = "https://stream.watsonplatform.net/speech-to-text/api/v1/customizations"
+uri = url+"/v1/customizations"
 r = requests.get(uri, auth=(username,password), verify=False, headers=headers)
 print "Get models returns: ", r.status_code
 print r.text
